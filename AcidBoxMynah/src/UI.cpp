4203e36eb8cba007fbaf52f88701d5d0b7f9317d
MoShang <moshangmusic@yahoo.com>
2026-06-27 11:18:25 +0800

    Adding UI support

---
+++ AcidBoxMynah/src/UI.cpp
@@ -0,0 +1,4 @@
+
+#include <Arduino.h>
+#include "general.h"
+#include "UI.h"
---
+++ AcidBoxMynah/src/UI.h
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef UI_H   
+#define UI_H   
+
+// Playback modes
+enum EditVoice {
+  Syn1,
+  Syn2,
+  Drm
+};
+
+// Synth edit modes
+enum SynthEditMode {
+  NoteEdit,
+  AccentEdit,
+  SlideEdit
+};
+
+// Synth edit modes
+enum SynthEditMode {
+  NoteEdit,
+  AccentEdit,
+  SlideEdit
+};
+
+
+#endif    
